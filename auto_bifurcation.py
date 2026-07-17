3ce53e000218d94f82feaa339af9dd215f25098a
Max <meantashi23@gmail.com>
2026-07-17 13:07:21 +0200

    SAF Update

--- auto_bifurcation.py
+++ auto_bifurcation.py
@@ -1,4 +1,20 @@
-import time
+import mido
+from mido import Message, MidiFile, MidiTrack
+
+def save_bifurcation_to_midi(bifurcation_points, filename="saf_bifurcation.mid"):
+    mid = MidiFile()
+    track = MidiTrack()
+    mid.tracks.append(track)
+    
+    # Mapping der Bifurkationspunkte auf MIDI-Noten (A-B linear aufgelöst)
+    for point in bifurcation_points:
+        # Hier wird die mathematische Instabilität in eine MIDI-Note übersetzt
+        note = int(60 + (point * 12)) 
+        track.append(Message('note_on', note=note, velocity=64, time=480))
+        track.append(Message('note_off', note=note, velocity=64, time=480))
+    
+    mid.save(filename)
+    print(f"MIDI-Struktur für {filename} erfolgreich generiert.")import time
 r = 3.5
 # Exponentieller Fokus auf den kritischen Bereich
 step = 0.001
